dcdb03b8eefa5d9a16f48942b76d59edef2a0c4e
Alexander Sunnerhage <alexsunnerhage@hotmail.com>
2024-03-07 14:06:12 +0000

    last

--- about/views.py
+++ about/views.py
@@ -5,7 +5,9 @@ from .forms import CollaborateForm
 
 
 def about_me(request):
-
+    """
+    render render.
+    """
     if request.method =="POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
